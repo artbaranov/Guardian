2ee047af8d06c34a985bcad48636bad0c38f8ef0
Artem Baranov <al.baranov@gmail.com>
2023-02-05 20:26:23 +0300

    Created Application file

---
+++ app/src/main/java/artembaranov/andorid_boilerplate/application/Application.kt
@@ -0,0 +1,10 @@
+package artembaranov.andorid_boilerplate.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Artem Baranov on 05.02.2023.
+ */
+@HiltAndroidApp
+class Application : Application()
