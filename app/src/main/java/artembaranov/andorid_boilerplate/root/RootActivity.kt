740ea5189b1f0327eca77bc6659c5d6d95607835
artembaranov <artbaranov@storytelling-software.com>
2023-02-06 00:02:19 +0300

    Implemented a base activity

--- app/src/main/java/artembaranov/andorid_boilerplate/root/RootActivity.kt
+++ app/src/main/java/artembaranov/andorid_boilerplate/root/RootActivity.kt
@@ -1,16 +1,32 @@
 package artembaranov.andorid_boilerplate.root
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import artembaranov.andorid_boilerplate.ui.navigation.BOILERPLATE_SCREEN_1
+import artembaranov.andorid_boilerplate.ui.navigation.navGraph
+import artembaranov.andorid_boilerplate.ui.theme.BoilerplateTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class RootActivity : AppCompatActivity() {
+class RootActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setContent {
-        }
         super.onCreate(savedInstanceState)
+
+        setContent {
+            BoilerplateTheme {
+                val navController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = BOILERPLATE_SCREEN_1 ,
+                ){
+                    navGraph(navController)
+                }
+            }
+        }
     }
 }
