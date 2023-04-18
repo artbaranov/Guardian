399a7ee2646226fa38dc03c956e0296a5d4b6147
artembaranov <artbaranov@storytelling-software.com>
2023-04-18 18:26:24 +0300

    WIP

--- app/src/main/java/artembaranov/guardian/ui/screens/home/views/ThreatItem.kt
+++ app/src/main/java/artembaranov/guardian/ui/screens/home/views/ThreatItem.kt
@@ -1,6 +1,7 @@
 package artembaranov.guardian.ui.screens.home.views
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -8,9 +9,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import artembaranov.guardian.entities.Threat
+import artembaranov.guardian.ui.previewValues.previewThreat
 
 @Composable
 fun ThreatItem(
@@ -24,10 +27,24 @@ fun ThreatItem(
             .padding(start = 35.dp, top = 13.dp, bottom = 12.dp, end = 25.dp)
             .fillMaxWidth(),
     ) {
-        Text(
-            text = threat.name,
-            fontSize = 18.sp,
-            fontFamily = FontFamily.SansSerif,
-        )
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = threat.name,
+                fontSize = 18.sp,
+                fontFamily = FontFamily.SansSerif,
+            )
+
+            Text(
+                text = threat.shortDescription,
+                fontSize = 12.sp,
+                fontFamily = FontFamily.SansSerif
+            )
+        }
     }
+}
+
+@Preview
+@Composable
+fun ThreatItem_Preview() {
+    ThreatItem(threat = previewThreat, onClick = { })
 }
