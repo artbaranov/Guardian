e5ded6109660cfd924ad6b094f78accbfa1f538b
artembaranov <artbaranov@storytelling-software.com>
2023-04-18 18:43:35 +0300

    WIP

--- app/src/main/java/artembaranov/guardian/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/artembaranov/guardian/ui/screens/home/HomeViewModel.kt
@@ -16,46 +16,14 @@ class HomeViewModel @Inject constructor() : ViewModel() {
         private set
 
     init {
-        uiState = uiState.copy(threats = stubThreatList())
+        uiState = uiState.copy(threats = loadThreats())
     }
 
-    private fun stubThreatList(): List<Threat> = listOf(
-        Threat(
-            id = 1,
-            name = "УБИ.001",
-            date = ZonedDateTime.parse("2020-03-19T10:24:00.000+03:00[Europe/Moscow]"),
-            shortDescription = "Угроза автоматического распространения вредоносного кода в грид-системе",
-            fullDescription = "Угроза заключается в возможности внедрения и запуска вредоносного кода от имени доверенного процесса на любом из ресурсных центров грид-системы и его автоматического распространения на все узлы грид-системы.\n" +
-                    "Данная угроза обусловлена слабостями технологии грид-вычислений – высоким уровнем автоматизации при малой администрируемости грид-системы.\n" +
-                    "Реализация данной угрозы возможна при условии наличия у нарушителя привилегий легального пользователя грид-системы",
-            threatSources = listOf(
-                ThreatSource("Внешний нарушитель со средним потенциалом"),
-                ThreatSource("Внутренний нарушитель со средним потенциалом")
-            ), threatConsequences = listOf(
-                ThreatConsequence("Нарушение конфиденциальности"),
-                ThreatConsequence("Нарушение целостности"),
-                ThreatConsequence("Нарушение доступности"),
-            )
-        ),
-        Threat(
-            id = 2,
-            name = "УБИ.001",
-            date = ZonedDateTime.parse("2020-03-19T10:24:00.000+03:00[Europe/Moscow]"),
-            shortDescription = "Угроза автоматического распространения вредоносного кода в грид-системе",
-            fullDescription = "Угроза заключается в возможности внедрения и запуска вредоносного кода от имени доверенного процесса на любом из ресурсных центров грид-системы и его автоматического распространения на все узлы грид-системы.\n" +
-                    "Данная угроза обусловлена слабостями технологии грид-вычислений – высоким уровнем автоматизации при малой администрируемости грид-системы.\n" +
-                    "Реализация данной угрозы возможна при условии наличия у нарушителя привилегий легального пользователя грид-системы",
-            threatSources = listOf(
-                ThreatSource("Внешний нарушитель со средним потенциалом"),
-                ThreatSource("Внутренний нарушитель со средним потенциалом")
-            ), threatConsequences = listOf(
-                ThreatConsequence("Нарушение конфиденциальности"),
-                ThreatConsequence("Нарушение целостности"),
-                ThreatConsequence("Нарушение доступности"),
-            )
-        ),
-        Threat(
-            id = 3,
+    private fun loadThreats(): List<Threat> {
+        val threats = mutableListOf<Threat>()
+        var id = 1L
+        val threat = Threat(
+            id = id,
             name = "УБИ.001",
             date = ZonedDateTime.parse("2020-03-19T10:24:00.000+03:00[Europe/Moscow]"),
             shortDescription = "Угроза автоматического распространения вредоносного кода в грид-системе",
@@ -71,5 +39,12 @@ class HomeViewModel @Inject constructor() : ViewModel() {
                 ThreatConsequence("Нарушение доступности"),
             )
         )
-    )
+
+        repeat(10) {
+            threats.add(threat)
+            id += 1
+        }
+
+        return threats
+    }
 }
