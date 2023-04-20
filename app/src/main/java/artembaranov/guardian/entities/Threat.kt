b90fcf3e8cd2fa19639ceb709d519ccf4f6e7a34
artembaranov <artbaranov@storytelling-software.com>
2023-04-21 01:11:17 +0300

    WIP

--- app/src/main/java/artembaranov/guardian/entities/Threat.kt
+++ app/src/main/java/artembaranov/guardian/entities/Threat.kt
@@ -3,11 +3,11 @@ package artembaranov.guardian.entities
 import java.time.ZonedDateTime
 
 data class Threat(
-//    val id: Long,
-//    val name: String,
-//    val date: ZonedDateTime,
-//    val shortDescription: String,
-//    val fullDescription: String,
+    val id: Long,
+    val name: String,
+    val date: ZonedDateTime,
+    val shortDescription: String,
+    val fullDescription: String,
     val threatSources: List<ThreatSource>,
     val objectsOfInfluence: List<ObjectOfInfluence>,
     val threatConsequences: List<ThreatConsequence>,
