f9f5f8512f2b8ee26242c8ab7111d5ec7800b747
Artem Baranov <al.baranov@gmail.com>
2023-04-21 05:47:46 +0300

    Simple ThreatRepository implementation

---
+++ app/src/main/java/artembaranov/guardian/repositories/ThreatRepository.kt
@@ -0,0 +1,37 @@
+package artembaranov.guardian.repositories
+
+import artembaranov.guardian.db.dao.ThreatDao
+import artembaranov.guardian.db.entities.Threat
+import javax.inject.Inject
+
+/**
+ * Created by Artem Baranov on 21.04.2023.
+ */
+
+interface ThreatRepository {
+    fun loadAll(): List<Threat>
+    fun insert(threat: Threat): Long
+    fun update(threat: Threat)
+    fun delete(threat: Threat)
+}
+
+class ThreatRepositoryImpl @Inject constructor(
+    private val threatDao: ThreatDao
+) : ThreatRepository {
+
+    override fun loadAll(): List<Threat> {
+        return threatDao.getAll()
+    }
+
+    override fun insert(threat: Threat): Long {
+        return threatDao.insert(threat)
+    }
+
+    override fun update(threat: Threat) {
+        threatDao.update(threat)
+    }
+
+    override fun delete(threat: Threat) {
+        threatDao.delete(threat)
+    }
+}
