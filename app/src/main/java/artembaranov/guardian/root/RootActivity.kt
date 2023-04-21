d420c14cee9c22808744925007b2205009a3e538
Artem Baranov <al.baranov@gmail.com>
2023-04-21 14:16:48 +0300

    Added search bar view

--- app/src/main/java/artembaranov/guardian/root/RootActivity.kt
+++ app/src/main/java/artembaranov/guardian/root/RootActivity.kt
@@ -3,9 +3,16 @@ package artembaranov.guardian.root
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import artembaranov.guardian.ui.components.BottomNavigationBar
@@ -31,6 +38,12 @@ class RootActivity : ComponentActivity() {
                 Scaffold(
                     bottomBar = {
                         BottomNavigationBar(navController = navController, navigationItems = items)
+                    },
+                    topBar = {
+                        TopAppBar(contentPadding = PaddingValues(end = 25.dp)) {
+                            Spacer(modifier = Modifier.weight(1f))
+                            Icon(modifier = Modifier, imageVector = Icons.Filled.Search, contentDescription = null)
+                        }
                     }
                 ) { innerPadding ->
                     NavHost(navController, startDestination = Screen.Home.route, Modifier.padding(innerPadding)) {
