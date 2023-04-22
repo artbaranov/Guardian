d06a6c94d6897cef43f0ed9b5ad451cec405e17a
Artem Baranov <al.baranov@gmail.com>
2023-04-22 20:48:21 +0300

    Implemented hiding bars

--- app/src/main/java/artembaranov/guardian/root/RootActivity.kt
+++ app/src/main/java/artembaranov/guardian/root/RootActivity.kt
@@ -3,6 +3,9 @@ package artembaranov.guardian.root
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
@@ -11,11 +14,17 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import artembaranov.guardian.ui.components.BottomNavigationBar
+import artembaranov.guardian.ui.navigation.HOME
 import artembaranov.guardian.ui.navigation.Screen
 import artembaranov.guardian.ui.navigation.navGraph
 import artembaranov.guardian.ui.theme.GuardianTheme
@@ -35,14 +44,46 @@ class RootActivity : ComponentActivity() {
                     Screen.Home,
                 )
 
+                val bottomBarVisible = remember { mutableStateOf(true) }
+                val topBarVisible = rememberSaveable { mutableStateOf(true) }
+
+                val backStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestinationRoute = backStackEntry?.destination?.route
+
+                when (currentDestinationRoute) {
+                    HOME -> {
+                        bottomBarVisible.value = true
+                        topBarVisible.value = true
+                    }
+
+                    "THREAT/{threatId}" -> {
+                        bottomBarVisible.value = false
+                        topBarVisible.value = false
+                    }
+                }
+
                 Scaffold(
                     bottomBar = {
-                        BottomNavigationBar(navController = navController, navigationItems = items)
+                        AnimatedVisibility(
+                            visible = bottomBarVisible.value,
+                            enter = slideInVertically(initialOffsetY = { it }),
+                            exit = slideOutVertically(targetOffsetY = { it }),
+                        ) {
+                            BottomNavigationBar(navController = navController, navigationItems = items)
+                        }
                     },
                     topBar = {
-                        TopAppBar(contentPadding = PaddingValues(end = 25.dp)) {
-                            Spacer(modifier = Modifier.weight(1f))
-                            Icon(modifier = Modifier, imageVector = Icons.Filled.Search, contentDescription = null)
+                        AnimatedVisibility(
+                            visible = topBarVisible.value,
+                            enter = slideInVertically(initialOffsetY = { -it }),
+                            exit = slideOutVertically(targetOffsetY = { -it }),
+                        ) {
+                            TopAppBar(
+                                contentPadding = PaddingValues(end = 25.dp),
+                            ) {
+                                Spacer(modifier = Modifier.weight(1f))
+                                Icon(modifier = Modifier, imageVector = Icons.Filled.Search, contentDescription = null)
+                            }
                         }
                     }
                 ) { innerPadding ->
