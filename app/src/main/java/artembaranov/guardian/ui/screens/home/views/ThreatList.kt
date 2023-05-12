4ccbf0151d1b6564be3b6fe45d486f88b84b0aed
Artem Baranov <al.baranov@gmail.com>
2023-05-13 02:15:03 +0300

    Implemented FAB

--- app/src/main/java/artembaranov/guardian/ui/screens/home/views/ThreatList.kt
+++ app/src/main/java/artembaranov/guardian/ui/screens/home/views/ThreatList.kt
@@ -1,20 +1,69 @@
 package artembaranov.guardian.ui.screens.home.views
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import artembaranov.guardian.entities.Threat
+import kotlinx.coroutines.launch
 
 @Composable
 fun ThreatList(
     threats: List<Threat>, modifier: Modifier = Modifier,
     onThreatClicked: (Threat) -> Unit
 ) {
-    LazyColumn(modifier = modifier) {
-        items(threats) { threat ->
-            ThreatItem(threat, Modifier.fillMaxWidth()) { onThreatClicked(threat) }
+    val listState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    var a by remember { mutableStateOf(0) }
+
+    Box(modifier = modifier) {
+        LazyColumn(
+            modifier = modifier,
+            state = listState
+        ) {
+            itemsIndexed(threats) { index, threat ->
+                ThreatItem(threat, Modifier.fillMaxWidth()) { onThreatClicked(threat) }
+
+                a = index
+            }
+        }
+
+        AnimatedVisibility(
+            enter = fadeIn(),
+            exit = fadeOut(),
+            visible = a >= 20,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 15.dp, end = 15.dp)
+        ) {
+            FloatingActionButton(
+                onClick = {
+                    coroutineScope.launch {
+                        listState.animateScrollToItem(index = 0)
+                    }
+                }
+            ) {
+                Icon(imageVector = Icons.Default.KeyboardArrowUp, contentDescription = null)
+            }
         }
     }
 }
